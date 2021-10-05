cb4c700e5c04e04190debe1fb9804554a499a7f7
Abdullah A. Noman <43316038+noman-nahil@users.noreply.github.com>
2021-10-05 21:44:10 +0600

    mongoose schema

--- index.js
+++ index.js
@@ -2,6 +2,24 @@ const mongoose = require('mongoose');
 
 
 //1st parameter -> connection path
-mongoose.connect('mongodb://localhost:27017/my-students', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb://localhost:27017/my-students', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
     .then(() => console.log("Connected to MongoDB Successfully"))
     .catch(err => console.error("Connection Failed!!"));
+
+const studentSchema = new mongoose.Schema({
+    firstName: String,
+    lastName: String,
+    dob: Date,
+    entryDate: { type: Date, default: Date.now },
+    passed: Boolean,
+    hobbies: [String],
+    parents: {
+        father: String,
+        mother: String,
+    },
+    subjects: [{ name: String, marks: { type: Number, min: 0, max: 100 } }]
+
+});
